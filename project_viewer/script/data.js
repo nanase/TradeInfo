e69afdddac12dfa8d77aa7f1436051422e739df4
Tomona Nanase <nanase@users.noreply.github.com>
2013-10-21 22:51:29 +0900

    getImageNameの修正とデータ部分の分離

---
+++ project_viewer/script/data.js
@@ -0,0 +1,25 @@
+/* Enchants */
+
+var enchants = new Array();
+enchants[0] = "ダメージ軽減";
+enchants[1] = "火炎耐性";
+enchants[2] = "落下耐性";
+enchants[3] = "爆発耐性";
+enchants[4] = "飛び道具耐性";
+enchants[5] = "水中呼吸";
+enchants[6] = "水中採掘";
+enchants[7] = "棘の鎧";
+enchants[16] = "ダメージ増加";
+enchants[17] = "アンデッド特効";
+enchants[18] = "虫特効";
+enchants[19] = "ノックバック";
+enchants[20] = "火属性";
+enchants[21] = "ドロップ増加";
+enchants[32] = "効率強化";
+enchants[33] = "シルクタッチ";
+enchants[34] = "耐久力";
+enchants[35] = "幸運";
+enchants[48] = "ダメージ増加";
+enchants[49] = "パンチ";
+enchants[50] = "フレイム";
+enchants[51] = "無限";
