1f7bf4a60d9b2c1b26c8ae889e24a4e3ea287194
Tomona Nanase <nanase@users.noreply.github.com>
2014-10-05 00:44:41 +0900

    EnchantInfoクラスにJavaDocを追加

--- project_plugin/src/net/nanase/minecraft/EnchantInfo.java
+++ project_plugin/src/net/nanase/minecraft/EnchantInfo.java
@@ -2,10 +2,26 @@ package net.nanase.minecraft;
 
 import net.minecraft.server.v1_7_R3.NBTTagCompound;
 
+/**
+ * エンチャント情報を格納します。
+ */
 public class EnchantInfo {
+    /**
+     * エンチャントID。
+     */
     public short i;
+
+    /**
+     * エンチャントレベル。
+     */
     public short l;
 
+    /**
+     * オブジェクトから情報を抽出します。
+     *
+     * @param enchant 抽出される NBTTagCompound オブジェクト。
+     * @return 抽出に成功した場合は true、 失敗した場合はfalse。
+     */
     public boolean extract(NBTTagCompound enchant) {
         this.i = enchant.getShort("id");
         this.l = enchant.getShort("lvl");
