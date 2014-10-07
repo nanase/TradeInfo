ae0b130f2c5341d7c0d1f1fdd446ece3891a7869
Tomona Nanase <nanase@users.noreply.github.com>
2014-10-07 23:20:26 +0900

    RecipeInfoクラスにJavaDocを追加

--- project_plugin/src/net/nanase/minecraft/RecipeInfo.java
+++ project_plugin/src/net/nanase/minecraft/RecipeInfo.java
@@ -2,13 +2,41 @@ package net.nanase.minecraft;
 
 import net.minecraft.server.v1_7_R3.NBTTagCompound;
 
+/**
+ * 取引の内容を格納します。
+ */
 public class RecipeInfo {
+    /**
+     * 取引の最大可能上限数。
+     */
     public int m;
+
+    /**
+     * 取引の回数。
+     */
     public int u;
+
+    /**
+     * 1つ目の買取アイテム。
+     */
     public ItemInfo a;
+
+    /**
+     * 2つ目の買取アイテム。
+     */
     public ItemInfo b;
+
+    /**
+     * 売出アイテム。
+     */
     public ItemInfo s;
 
+    /**
+     * オブジェクトから情報を抽出します。
+     *
+     * @param recipe 抽出される NBTTagCompound オブジェクト。
+     * @return 抽出に成功した場合は true、 失敗した場合はfalse。
+     */
     public boolean extract(NBTTagCompound recipe) {
         this.m = recipe.getInt("maxUses");
         this.u = recipe.getInt("uses");
