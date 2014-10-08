7c7469b1fabecf7ba01bbf5e4da1f2e5f09e5185
Tomona Nanase <nanase@users.noreply.github.com>
2014-10-08 22:50:44 +0900

    Utilsクラスを追加

---
+++ project_plugin/src/net/nanase/minecraft/Utils.java
@@ -0,0 +1,18 @@
+package net.nanase.minecraft;
+
+/**
+ * Project: TradeInfo
+ * Created by nanase on 14/10/08.
+ */
+
+public class Utils {
+    /**
+     * 指定された数値の平方数を求めます。
+     *
+     * @param x 数値。
+     * @return パラメータ x の平方数。
+     */
+    public static double pow2(double x) {
+        return x * x;
+    }
+}
